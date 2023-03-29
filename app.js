483cf95b3a6faca9c9d31d5ee652b8f203a5328d
Jessica Villa <suhkha92@gmail.com>
2023-03-28 18:03:06 -0600

    setup server with express

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("starting...");
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
