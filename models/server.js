ab7ed6991e0fb13d8af97acfe6f368b306b1ad6e
Jessica Villa <suhkha92@gmail.com>
2023-04-03 14:24:54 -0600

    database configuration

--- models/server.js
+++ models/server.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 
+const { databaseConnection } = require("../database/config");
+
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
     this.usersPath = "/api/users";
 
+    //database
+    this.database();
+
     //middleware
     this.middlewares();
 
     //routes
     this.routes();
+  }
+
+  async database() {
+    await databaseConnection();
   }
 
   middlewares() {
