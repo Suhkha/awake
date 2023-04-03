f778f9ca1ba757c0d60b2ce4f3d05b3cbaf39a8b
Jessica Villa <suhkha92@gmail.com>
2023-04-03 11:42:54 -0600

    query params, post and put functions

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,32 @@
 const { response } = require("express");
 
 const usersGet = (req, res = response) => {
+  const { q, name = "no name", lastname } = req.query;
+
   res.json({
     message: "get users",
+    q,
+    name,
+    lastname,
   });
 };
 
 const usersPost = (req, res = response) => {
+  const { name, bias } = req.body;
+
   res.json({
     message: "post users",
+    name,
+    bias,
   });
 };
 
 const usersPut = (req, res = response) => {
+  const { id } = req.params;
+
   res.json({
     message: "put users",
+    id,
   });
 };
 
--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ router.get("/", usersGet);
 
 router.post("/", usersPost);
 
-router.put("/", usersPut);
+router.put("/:id", usersPut);
 
 router.delete("/", usersDelete);
 
