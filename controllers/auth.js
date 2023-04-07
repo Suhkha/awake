168ede47c8f2c6ca396057d6753c8c2b3d705ce5
Jessica Villa <suhkha92@gmail.com>
2023-04-07 14:04:30 -0600

    validations in login controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,38 @@
 const { response } = require("express");
+const User = require("../models/user");
+const bcryptjs = require("bcryptjs");
 
-const login = (req, res = response) => {
-  res.json({
-    message: "login",
-  });
+const login = async (req, res = response) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if email exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(500).json({
+        message: "Something wrong with email or password --email",
+      });
+    }
+
+    //Check if user is active in database
+    if (!user.status) {
+      return res.status(400).json({
+        message: "Something wrong with email or password --status: false",
+      });
+    }
+
+    //Validate password
+    const validPassword = bcryptjs.compareSync(password, user.password);
+    if (!validPassword) {
+      return res.status(500).json({
+        message: "Something wrong with email or password --password",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something wrong",
+    });
+  }
 };
 
 module.exports = {
