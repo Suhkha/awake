eafe6d4b957a0a2047219f2aa03981c5a38906af
Jessica Villa <suhkha92@gmail.com>
2023-04-07 13:10:06 -0600

    login route and basic validations for auth

--- models/server.js
+++ models/server.js
@@ -8,6 +8,7 @@ class Server {
     this.app = express();
     this.port = process.env.PORT;
     this.usersPath = "/api/users";
+    this.authPath = "/api/auth";
 
     //database
     this.database();
@@ -30,6 +31,7 @@ class Server {
   }
 
   routes() {
+    this.app.use(this.authPath, require("../routes/auth"));
     this.app.use(this.usersPath, require("../routes/users"));
   }
 
