e5cf8a1452b48c0040f8f70ae52cd720710cbdba
Jessica Villa <suhkha92@gmail.com>
2023-04-10 17:50:15 -0600

    optimize middleware: barrel file

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { validateFields } = require("../middlewares/validate-fields");
-const { validateJWT } = require("../middlewares/validate-jwt");
-const { validateAdminRole, hasRole } = require("../middlewares/validate-roles");
+const {
+  validateFields,
+  validateJWT,
+  validateAdminRole,
+  hasRole,
+} = require("../middlewares");
+
 const {
   isValidRole,
   isValidEmail,
